411c4d9c55b10c6deb7605da35eaf2ec9f303a49
Admin <10421093@student.vgu.edu.vn>
2025-06-18 11:03:56 +0700

    add cache

---
+++ src/main/java/com/example/demoSQL/config/CacheConfig.java
@@ -0,0 +1,22 @@
+package com.example.demoSQL.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeine() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, java.util.concurrent.TimeUnit.MINUTES)
+                .maximumSize(1000);
+    }
+    @Bean
+    public CacheManager cacheManger(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}
